7de18ce33f442dbaf9dd2e269743c6ba69011d55
arigatory <arigatory@gmail.com>
2019-10-31 16:39:46 +0300

    Add project files.

---
+++ dotNETtask2.2/Body3D.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNETtask2._2
+{
+    abstract class Body3D
+    {
+        
+    }
+}
