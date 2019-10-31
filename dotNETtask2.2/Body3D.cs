317870b99ba4fffc3132c9f04a823ca09897298a
arigatory <arigatory@gmail.com>
2019-10-31 16:46:51 +0300

    create simple classes

--- dotNETtask2.2/Body3D.cs
+++ dotNETtask2.2/Body3D.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,16 @@ namespace dotNETtask2._2
 {
     abstract class Body3D
     {
-        
+        public Body3D(string name = "NoName")
+        {
+            PetName = name;
+        }
+
+        public string PetName { get; set; }
+
+        public virtual void Draw()
+        {
+            Console.WriteLine("Body draw()");
+        }
     }
 }
